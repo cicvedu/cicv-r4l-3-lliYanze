3759313f77770826c654cf6da9e39771b5ab3e36
李彦泽 <1310469009@qq.com>
2024-07-15 22:13:59 +0800

    [feat]: support module json

--- linux/scripts/generate_rust_analyzer.py
+++ linux/scripts/generate_rust_analyzer.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: GPL-2.0
-"""generate_rust_analyzer - Generates the `rust-project.json` file for `rust-analyzer`.
-"""
+"""generate_rust_analyzer - Generates the `rust-project.json` file for `rust-analyzer`."""
 
 import argparse
 import json
 import logging
+import os
 import pathlib
 import sys
 
-def generate_crates(srctree, objtree, sysroot_src):
+
+def generate_crates(srctree, objtree, sysroot_src, external_src):
     # Generate the configuration list.
     cfg = []
     with open(objtree / "include" / "generated" / "rustc_cfg") as fd:
@@ -24,20 +25,27 @@ def generate_crates(srctree, objtree, sysroot_src):
     crates = []
     crates_indexes = {}
 
-    def append_crate(display_name, root_module, deps, cfg=[], is_workspace_member=True, is_proc_macro=False):
+    def append_crate(
+        display_name,
+        root_module,
+        deps,
+        cfg=[],
+        is_workspace_member=True,
+        is_proc_macro=False,
+    ):
         crates_indexes[display_name] = len(crates)
-        crates.append({
-            "display_name": display_name,
-            "root_module": str(root_module),
-            "is_workspace_member": is_workspace_member,
-            "is_proc_macro": is_proc_macro,
-            "deps": [{"crate": crates_indexes[dep], "name": dep} for dep in deps],
-            "cfg": cfg,
-            "edition": "2021",
-            "env": {
-                "RUST_MODFILE": "This is only for rust-analyzer"
+        crates.append(
+            {
+                "display_name": display_name,
+                "root_module": str(root_module),
+                "is_workspace_member": is_workspace_member,
+                "is_proc_macro": is_proc_macro,
+                "deps": [{"crate": crates_indexes[dep], "name": dep} for dep in deps],
+                "cfg": cfg,
+                "edition": "2021",
+                "env": {"RUST_MODFILE": "This is only for rust-analyzer"},
             }
-        })
+        )
 
     # First, the ones in `rust/` since they are a bit special.
     append_crate(
@@ -65,8 +73,7 @@ def generate_crates(srctree, objtree, sysroot_src):
         [],
         is_proc_macro=True,
     )
-    crates[-1]["proc_macro_dylib_path"] = "rust/libmacros.so"
-
+    crates[-1]["proc_macro_dylib_path"] = f"{objtree}/rust/libmacros.so"
     append_crate(
         "build_error",
         srctree / "rust" / "build_error.rs",
@@ -75,7 +82,7 @@ def generate_crates(srctree, objtree, sysroot_src):
 
     append_crate(
         "bindings",
-        srctree / "rust"/ "bindings" / "lib.rs",
+        srctree / "rust" / "bindings" / "lib.rs",
         ["core"],
         cfg=cfg,
     )
@@ -88,54 +95,59 @@ def generate_crates(srctree, objtree, sysroot_src):
         cfg=cfg,
     )
     crates[-1]["source"] = {
-        "include_dirs": [
-            str(srctree / "rust" / "kernel"),
-            str(objtree / "rust")
-        ],
+        "include_dirs": [str(srctree / "rust" / "kernel"), str(objtree / "rust")],
         "exclude_dirs": [],
     }
+
+    def is_root_crate(build_file, target):
+        return os.path.exists(build_file) and target in open(build_file).read()
 
     # Then, the rest outside of `rust/`.
     #
     # We explicitly mention the top-level folders we want to cover.
-    for folder in ("samples", "drivers"):
+    for folder in ("samples", "drivers") if external_src is None else [external_src]:
         for path in (srctree / folder).rglob("*.rs"):
             logging.info("Checking %s", path)
             name = path.name.replace(".rs", "")
 
             # Skip those that are not crate roots.
-            if f"{name}.o" not in open(path.parent / "Makefile").read():
-                continue
-
-            logging.info("Adding %s", name)
-            append_crate(
-                name,
-                path,
-                ["core", "alloc", "kernel"],
-                cfg=cfg,
-            )
+            if is_root_crate(path.parent / "Makefile", f"{name}.o") or is_root_crate(
+                path.parent / "Kbuild", f"{name}.o"
+            ):
+                logging.info("Adding %s", name)
+                append_crate(
+                    name,
+                    path,
+                    ["core", "alloc", "kernel"],
+                    cfg=cfg,
+                )
 
     return crates
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--verbose', '-v', action='store_true')
+    parser.add_argument("--verbose", "-v", action="store_true")
     parser.add_argument("srctree", type=pathlib.Path)
     parser.add_argument("objtree", type=pathlib.Path)
     parser.add_argument("sysroot_src", type=pathlib.Path)
+    parser.add_argument("exttree", type=pathlib.Path, nargs="?")
     args = parser.parse_args()
 
     logging.basicConfig(
         format="[%(asctime)s] [%(levelname)s] %(message)s",
-        level=logging.INFO if args.verbose else logging.WARNING
+        level=logging.INFO if args.verbose else logging.WARNING,
     )
 
     rust_project = {
-        "crates": generate_crates(args.srctree, args.objtree, args.sysroot_src),
+        "crates": generate_crates(
+            args.srctree, args.objtree, args.sysroot_src, args.exttree
+        ),
         "sysroot_src": str(args.sysroot_src),
     }
 
     json.dump(rust_project, sys.stdout, sort_keys=True, indent=4)
+
 
 if __name__ == "__main__":
     main()
