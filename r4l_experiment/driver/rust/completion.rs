54083beb58935a9545e4ab4707e7912ab1a91473
李彦泽 <1310469009@qq.com>
2024-07-20 16:31:04 +0800

    [feat]: cdev init
    * load_module.sh can make /dev/completion node

---
+++ r4l_experiment/driver/rust/completion.rs
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: GPL-2.0
+
+//! Rust character device sample.
+
+use core::result::Result::Err;
+
+use kernel::prelude::*;
+use kernel::sync::Mutex;
+use kernel::{chrdev, file};
+
+const GLOBALMEM_SIZE: usize = 0x1000;
+
+module! {
+    type: Completion,
+    name: "rust_chrdev",
+    author: "Rust for Linux Contributors",
+    description: "Rust character device sample",
+    license: "GPL",
+}
+
+static GLOBALMEM_BUF: Mutex<[u8; GLOBALMEM_SIZE]> = unsafe { Mutex::new([0u8; GLOBALMEM_SIZE]) };
+
+struct RustFile {
+    #[allow(dead_code)]
+    inner: &'static Mutex<[u8; GLOBALMEM_SIZE]>,
+}
+#[vtable]
+impl file::Operations for RustFile {
+    type Data = Box<Self>;
+
+    fn open(_shared: &(), _file: &file::File) -> Result<Box<Self>> {
+        Ok(Box::try_new(RustFile {
+            inner: &GLOBALMEM_BUF,
+        })?)
+    }
+
+    fn write(
+        this: &Self,
+        _file: &file::File,
+        reader: &mut impl kernel::io_buffer::IoBufferReader,
+        offset: u64,
+    ) -> Result<usize> {
+        pr_info!("write to device\n");
+        let offset = offset.try_into()?;
+        let mut buffer = this.inner.lock();
+        let len = core::cmp::min(reader.len(), buffer.len() - offset as usize);
+        reader.read_slice(&mut buffer[offset..][..len])?;
+        core::result::Result::Ok(len)
+    }
+
+    fn read(
+        this: &Self,
+        _file: &file::File,
+        writer: &mut impl kernel::io_buffer::IoBufferWriter,
+        offset: u64,
+    ) -> Result<usize> {
+        pr_info!("read for device\n");
+        let offset = offset.try_into()?;
+        let buffer = this.inner.lock();
+        let len = core::cmp::min(writer.len(), buffer.len() - offset as usize);
+        writer.write_slice(&buffer[offset..][..len])?;
+        core::result::Result::Ok(len)
+    }
+}
+
+struct Completion {
+    _dev: Pin<Box<chrdev::Registration<2>>>,
+}
+
+impl kernel::Module for Completion {
+    fn init(name: &'static CStr, module: &'static ThisModule) -> Result<Self> {
+        pr_info!("Rust character device sample (init)\n");
+
+        let mut chrdev_reg = chrdev::Registration::new_pinned(name, 0, module)?;
+
+        // Register the same kind of device twice, we're just demonstrating
+        // that you can use multiple minors. There are two minors in this case
+        // because its type is `chrdev::Registration<2>`
+        chrdev_reg.as_mut().register::<RustFile>()?;
+        chrdev_reg.as_mut().register::<RustFile>()?;
+
+        Ok(Completion { _dev: chrdev_reg })
+    }
+}
+
+impl Drop for Completion {
+    fn drop(&mut self) {
+        pr_info!("Rust character device sample (exit)\n");
+    }
+}
